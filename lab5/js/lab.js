505bd28ad62ad2f744bb6b7a02dd54a4308acc72
RyanP <rpalmber@ucsc.edu>
2021-10-18 19:12:56 -0700

    Lab 5 and other minor adjustments

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/*
+ *Summary: A simple .js script that uses different variables t calculate the age of a car.
+ *Description: Takes the release year and subtracts it from the current year.
+ *@link: https://ryanpalmberg.github.io/art101/
+ *@file: This file details the car info
+ *@Author: Ryan Palmberg <rpalmber@ucsc.edu>
+ *@since  10.18.21
+*/
+
+//Define Variables
+make = "Toyota";
+model = "4Runner";
+color = "Millenium Silver";
+year = 2000;
+
+//calculate
+age = 2021-year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
