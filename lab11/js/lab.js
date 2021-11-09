7035b1c20668712c935fca41be39da7cc00234c0
RyanP <rpalmber@ucsc.edu>
2021-11-08 20:43:13 -0800

    Fixing things up a bit

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -1,35 +1,31 @@
 /*
- *Summary: .js script that scrambles text input by user.
- *Description: Takes both the button for scrambling and the input method and returns a string on the page itself.
+ *Summary: .js script that Lets the html buttons toggle between css styles.
+ *Description: Takes css styles as arguments and switches between them using id's and classes.
  *@link: https://ryanpalmberg.github.io/art101/
  *@file: ordinary .js file.
  *@Author: Ryan Palmberg <rpalmber@ucsc.edu>
- *@since  11.3.21
+ *@since  11.8.21
 */
 
-// var button1 = $("big");
-// var button2 = $("Button2");
-// var button3 = $("Button3");
-
-
+// The first button function
 $("button.button1").click(function(){
+  //Switches off the current class being used to style the targeted text.
   $("#challengetarget").toggleClass("smol")
+  //Turns on the new stylization at the same time the first is turned off.
   $("#challengetarget").toggleClass("big");
 });
 
+//Repeat
 $("button.button2").click(function(){
   $("#challengetarget2").toggleClass("boring")
   $("#challengetarget2").toggleClass("fancy");
 });
 
+//Repeat
 $("button.button3").click(function(){
   $("#challengetarget3").toggleClass("eh")
   $("#challengetarget3").toggleClass("verysmol");
 });
 
-
-
-
-// $("#feed").click(function(){
-//   $("button1").parent().toggleClass("special");
-// }))
+//The use of toggle for both styles allows for it to be switched back even if
+//the styles share similar properties that would ordinarily cancel each other out.
